dcd6b307fcedb215154da36a6b2faa8e42281235
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-11-29 08:05:03 +0100

    Async thunk for adding and removing a contact- actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -4,14 +4,38 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 axios.defaults.baseURL = 'https://64d9fc3ce947d30a260a982f.mockapi.io';
 
 export const fetchContacts = createAsyncThunk(
-    .contacts/fetchContacts',
-    async (_, thunkAPI) => {
-        try {
-            const response = await axios.get('/contacts');
-            return response.data;
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error.toString());
-        }
-    },
+  'contacts/fetchContacts',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.toString());
+    }
+  }
 );
 
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async ({ name, number }, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts', { name, number });
+      console.log(response);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.toString());
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (id, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${id}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.toString());
+    }
+  }
+);
