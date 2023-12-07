4f81e450e32b8776ea25aaf062029704d9c8c5fb
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-12-07 06:37:20 +0100

    Add authentication thunks using axios
    - Set up functions for user registration, login, logout, and data refresh
    - Utilize axios for making asynchronous requests
    - Handle authentication headers for HTTP requests

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 
 import './App.css';
-import ContactForm from './ContactForm/ContactForm';
+import ContactForm from './ContactForm/ContactFormsss';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
-import { fetchContacts } from '../redux/actions';
+import { fetchContacts } from '';
 
 export const App = () => {
   const dispatch = useDispatch();
